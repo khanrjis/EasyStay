c6c53edbdbe8566d3af6f69192d6c9de0211972d
Arman <155127929+khanrjis@users.noreply.github.com>
2026-02-15 01:58:25 +0530

    Change server port and MongoDB connection settings
    Updated server port to 5000 and modified MongoDB connection options.

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 require("dotenv").config(); // load env first
-
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
+
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-
+///
 const Listing = require("./models/listings");
 const Review = require("./models/review");
 const User = require("./models/user");
-
+//roues
 const listingRoutes = require("./routes/listings");
 const userRoutes = require("./routes/user.js");
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT ||5000;
 
 const MONGO_URL = process.env.MONGO_URL;
 
@@ -29,11 +30,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
+//
 app.engine("ejs", ejsmate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+ 
 const store = MongoStore.create({
   mongoUrl :  process.env.MONGO_URL,
   crypto:{
@@ -55,32 +57,41 @@ const sessionOption = {
       httpOnly: true,
     },
   }
+
+
+// Session + Flash (must be registered before Passport)
 app.use(session(sessionOption));
 app.use(flash());
+
 // Passport config
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Flash messages
-app.use(flash());
+//
 app.use((req, res, next) => {
   res.locals.currUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  
   next();
 });
 
 // Routes
 app.use("/listings", listingRoutes);
 app.use("/", userRoutes);
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 // MongoDB connection
 const connectDb = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL, {
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
